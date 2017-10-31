7d4560f695d16223e20322aa7ba43dab808d5dfa
bsarvaiya <bsarvaiya@officebrain.com>
2017-10-31 15:03:16 +0530

    scheduler and worker modules created and implementation examples are given in example folder

--- JobQue/example/worker/start_worker.js
+++ JobQue/example/worker/start_worker.js
@@ -3,4 +3,8 @@ const cxnOptions = app.get('rethinkdb')
 var qOptions = app.get('jOptions')
 qOptions.name = 'start_worker'
 
-const worker = require('./worker')(cxnOptions, qOptions)
+const worker = require('../../worker/worker')
+const start_worker = new worker({
+  cxnOptions: cxnOptions,
+  qOptions: qOptions
+})
