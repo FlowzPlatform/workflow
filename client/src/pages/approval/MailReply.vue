7e0a0e90eff9bef8e04b27f3a4dc1d5f85b7fb87
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2017-11-23 18:10:40 +0530

    implement send mail and mail reply

--- client/src/pages/approval/MailReply.vue
+++ client/src/pages/approval/MailReply.vue
@@ -17,6 +17,7 @@
 </template>
 <script>
 import flowInstance from '../../api/flowzinstance'
+import config from '../../config'
 import _ from 'lodash'
 import axios from 'axios'
 import $ from 'jquery'
@@ -27,57 +28,244 @@ export default {
       Aloading: false,
       Rloading: false,
       notes: '',
+      input: [],
       flowInstanceLog: [],
-      viewTemplateHtml: ''
+      viewTemplateHtml: '',
+      process: '',
+      err: []
     }
   },
   methods: {
     async handleApproval (action) {
       let self = this
+      this.err = []
+      let err = this.err
       let lastLogIndex
+      let inputs = {}
       this.Aloading = true
-      await flowInstance.getThis(self.$route.params.fiid)
-        .then(async function (response) {
-          let process = await _.find(response.data.processList, ['id', self.$route.params.pid])
-          _.forEach(process.target, function (item) {
-            lastLogIndex = _.findIndex(response.data.process_log, self.getLastLog(item.id))
-            _.forEach(response.data.process_log[lastLogIndex].input, function (inputs) {
-              if (inputs.Email === self.$route.params.mailid) {
-                inputs.Approved = action
+      for (var i = 0; i < self.process.inputProperty[0].entityschema.entity.length; i++) {
+        let element = self.process.inputProperty[0].entityschema.entity[i].name
+        $('input[name="' + element.toLowerCase() + '"]').each(async function () {
+          if (self.process.inputProperty[0].entityschema.entity[i].property.optional === false) {
+            // inputs[element] = await this.value
+            let result = self.process.inputProperty[0].entityschema.entity[i].property
+            let emailRegEx = '(\\w+)\\@(\\w+)\\.[a-zA-Z]'
+            let numberRegEx = '^[0-9]+$'
+            let dateRegEx = '(0?[1-9]|[12]\\d|30|31)[^\\w\\d\\r\\n:](0?[1-9]|1[0-2])[^\\w\\d\\r\\n:](\\d{4}|\\d{2})'
+            let val = this.value
+            if (val === '' || val === null || val === undefined) {
+              err.push(element + ' - is required..!')
+              if ($('input[name="' + element + '"]').parent().next('.validation').length === 0) {
+                $('input[name="' + element + '"]').parent().after('<div class="validation" style="color:red;"> required..!</div>')
               }
+            } else {
+              $('input[name="' + element + '"]').parent().next('.validation').remove()
+            }
+
+            if (this.hasAttribute('pattern')) {
+              let pttrn = new RegExp(result.regEx)
+              let regEx = pttrn.test(val)
+              if (!regEx) {
+                err.push(element + ' - Enter proper format..!')
+                if ($('input[name="' + element + '"]').parent().next('.validation').length === 0) {
+                  $('input[name="' + element + '"]').parent().after('<div class="validation" style="color:red;"> Enter proper format..!</div>')
+                }
+              } else {
+                // obj[element] = val
+                $('input[name="' + element + '"]').parent().next('.validation').remove()
+              }
+            }
+            if (this.type === 'date') {
+              let inputDate = new Date(val)
+              if (result.maxdate !== '') {
+                let maxDate = new Date(result.maxdate)
+                if (inputDate > maxDate) {
+                  err.push(element + ' - Enter minimum date then ' + maxDate)
+                  if ($('input[name="' + element + '"]').parent().next('.validation').length === 0) {
+                    $('input[name="' + element + '"]').parent().after('<div class="validation" style="color:red;"> Enter minimum date then ' + maxDate + '</div>')
+                  }
+                } else {
+                  // obj[element] = val
+                  $('input[name="' + element + '"]').parent().next('.validation').remove()
+                }
+              } else if (result.mindate !== '') {
+                let minDate = new Date(result.mindate)
+                if (inputDate < minDate) {
+                  err.push(element + ' - Enter maximum date then ' + minDate)
+                  if ($('input[name="' + element + '"]').parent().next('.validation').length === 0) {
+                    $('input[name="' + element + '"]').parent().after('<div class="validation" style="color:red;"> Enter minimum date then ' + minDate + '</div>')
+                  }
+                } else {
+                  // obj[element] = val
+                  $('input[name="' + element + '"]').parent().next('.validation').remove()
+                }
+              }
+            }
+            if (result.min !== 0 || result.max !== 0) {
+              if (val.length > result.min && val.length > result.max) {
+                err.push(element + ' - Minimum length :' + result.min + ' Maximum length :' + result.max)
+                if ($('input[name="' + element + '"]').parent().next('.validation').length === 0) {
+                  $('input[name="' + element + '"]').parent().after('<div class="validation" style="color:red;"> Minimum length :' + result.min + ' Maximum length :' + result.max + '</div>')
+                }
+              } else if (val.length > result.max && val.length < result.min) {
+                err.push(element + ' - Minimum length :' + result.min + ' Maximum length :' + result.max)
+                if ($('input[name="' + element + '"]').parent().next('.validation').length === 0) {
+                  $('input[name="' + element + '"]').parent().after('<div class="validation" style="color:red;"> Minimum length :' + result.min + ' Maximum length :' + result.max + '</div>')
+                }
+              } else {
+                // obj[element] = val
+                $('input[name="' + element + '"]').parent().next('.validation').remove()
+              }
+            }
+            if (result.allowedValue.length > 0) {
+              let check = _.includes(result.allowedValue, val)
+              if (!check) {
+                err.push(element + ' - Allowed value are' + result.allowedValue)
+                if ($('input[name="' + element + '"]').parent().next('.validation').length === 0) {
+                  $('input[name="' + element + '"]').parent().after('<div class="validation" style="color:red;"> Allowed value are' + result.allowedValue + '</div>')
+                }
+              } else {
+                // obj[element] = val
+                $('input[name="' + element + '"]').parent().next('.validation').remove()
+              }
+            }
+            switch (this.type) {
+              case 'email':
+                let re = new RegExp(emailRegEx)
+                let testEmail = re.test(val)
+                if (testEmail) {
+                  inputs[element] = await val
+                  $('input[name="' + element + '"]').parent().next('.validation').remove()
+                } else {
+                  err.push(element + ' - Enter valid email address..!')
+                  if ($('input[name="' + element + '"]').parent().next('.validation').length === 0) {
+                    $('input[name="' + element + '"]').parent().after('<div class="validation" style="color:red;"> Enter valid email address..!</div>')
+                  }
+                }
+                break
+              case 'number':
+                re = new RegExp(numberRegEx)
+                testEmail = re.test(val)
+                if (testEmail) {
+                  inputs[element] = await val
+                  $('input[name="' + element + '"]').parent().next('.validation').remove()
+                } else {
+                  err.push(element + ' - Enter numbers only..!')
+                  if ($('input[name="' + element + '"]').parent().next('.validation').length === 0) {
+                    $('input[name="' + element + '"]').parent().after('<div class="validation" style="color:red;"> Enter numbers only..!</div>')
+                  }
+                }
+                break
+              case 'date':
+                re = new RegExp(dateRegEx)
+                testEmail = re.test(val)
+                if (testEmail) {
+                  inputs[element] = await val
+                  $('input[name="' + element + '"]').parent().next('.validation').remove()
+                } else {
+                  err.push(element + ' - Invalid date format..!')
+                  if ($('input[name="' + element + '"]').parent().next('.validation').length === 0) {
+                    $('input[name="' + element + '"]').parent().after('<div class="validation" style="color:red;"> Invalid date format..!</div>')
+                  }
+                }
+                break
+              default:
+                inputs[element] = await val
+            }
+          }
+        })
+      }
+      if (err.length > 0) {
+        self.$Notice.error({title: 'Error..!', desc: 'Fill all the details carefully.'})
+      } else {
+        self.input.inputs = inputs
+        console.log('input-->', self.input)
+        await flowInstance.getThis(self.$route.params.fiid)
+          .then(async function (response) {
+            // self.process = await _.find(response.data.processList, ['id', self.$route.params.pid])
+            _.forEach(self.process.target, function (item) {
+              lastLogIndex = _.findIndex(response.data.process_log, self.getLastLog(item.id))
+              _.forEach(response.data.process_log[lastLogIndex].input, function (inputs) {
+                if (inputs.Email === self.$route.params.mailid) {
+                  inputs.Approved = action
+                }
+              })
             })
+            await self.addToJobQue()
+            flowInstance.put(self.$route.params.fiid, response.data)
+              .then(async function (res) {
+                self.Aloading = false
+                self.$Notice.success({title: 'Approval confirmed...!'})
+              })
+              .catch(function (error) {
+                self.$Notice.error({title: 'Error..!', desc: 'Server is busy please try again after some time'})
+                console.log('Error..!', error)
+                self.Aloading = false
+              })
           })
-          flowInstance.put(self.$route.params.fiid, response.data)
-            .then(async function (res) {
-              self.Aloading = false
-              self.$Notice.success({title: 'Approval confirmed...!'})
-            })
-            .catch(function (error) {
-              self.$Notice.error({title: 'Error..!', desc: 'Server is busy please try again after some time'})
-              console.log('Error..!', error)
-              self.Aloading = false
-            })
+          .catch(function (error) {
+            self.$Notice.error({title: 'Error..!', desc: 'Action can not perform please try again...'})
+            console.log('Error..!', error)
+            self.Aloading = false
+          })
+      }
+    },
+    addToJobQue () {
+      let self = this
+      let lastLog = self.getLastLog(self.$route.params.pid)
+      console.log('1', lastLog)
+      if (lastLog !== undefined && lastLog.status === 'inputRequired') {
+        console.log('2')
+        let dataObject = {
+          'fId': self.$route.params.fiid,
+          'input': self.input,
+          'isExternalInput': true,
+          'jobId': lastLog.job
+        }
+        let uri = config.serverURI + '/addInputToJobQue'
+        console.log('dataObject', dataObject)
+        axios.post(uri, dataObject)
+        .then(function (response) {
+          console.log(response)
         })
         .catch(function (error) {
-          self.$Notice.error({title: 'Error..!', desc: 'Action can not perform please try again...'})
-          console.log('Error..!', error)
-          self.Aloading = false
+          console.log(error)
         })
+      }
     },
     getLastLog (item) {
       return _.chain(this.flowInstanceLog).orderBy(['lastModified'], ['asc']).findLast((f) => { return f.job === item }).value()
+    },
+    fillForm (log) {
+      for (var i = 0; i < this.process.inputProperty[0].entityschema.entity.length; i++) {
+        let element = this.process.inputProperty[0].entityschema.entity[i].name
+        let result = this.process.inputProperty[0].entityschema.entity[i].property
+        $('input[name="' + element.toLowerCase() + '"]').each(function () {
+          if (log.input[0][element] !== undefined) {
+            this.value = log.input[0][element]
+          }
+          if (result.type === 'date') {
+            this.setAttribute('type', 'date')
+          }
+          if (result.regEx !== '') {
+            if (this.type === 'text' || this.type === 'number' || this.type === 'phone' || this.type === 'date' || this.type === 'select-one') {
+              this.setAttribute('pattern', result.property.regEx)
+            }
+          }
+        })
+      }
     }
   },
   async mounted () {
     let self = this
-    let lastLogIndex, viewTemplateUrl
+    let lastLogIndex, viewTemplateUrl, processLog
 
-    flowInstance.getThis(self.$route.params.fiid)
+    await flowInstance.getThis(self.$route.params.fiid)
       .then(async function (response) {
         self.flowInstanceLog = await response.data.process_log
-        let process = await _.find(response.data.processList, ['id', self.$route.params.pid])
-        let processLog = await self.getLastLog(self.$route.params.pid)
-        _.forEach(process.target, async function (item) {
+        self.process = await _.find(response.data.processList, ['id', self.$route.params.pid])
+        processLog = await self.getLastLog(self.$route.params.pid)
+        _.forEach(self.process.target, async function (item) {
           lastLogIndex = await _.findIndex(response.data.process_log, self.getLastLog(item.id))
           _.forEach(response.data.process_log[lastLogIndex].input, function (inputs) {
             if (inputs.Email === self.$route.params.mailid) {
@@ -85,7 +273,7 @@ export default {
             }
           })
         })
-        viewTemplateUrl = _.find(process.inputProperty[0].entityschema.viewTemplate, ['filename', process.inputProperty[0].viewTemplate])
+        viewTemplateUrl = _.find(self.process.inputProperty[0].entityschema.viewTemplate, ['filename', self.process.inputProperty[0].viewTemplate])
         viewTemplateUrl = viewTemplateUrl.url
         await axios({
           method: 'get',
@@ -94,27 +282,13 @@ export default {
         .then(async function (res) {
           self.viewTemplateHtml = res.data
           self.viewTemplateHtml = await self.viewTemplateHtml.split('./assets/main.css').join('')
-          console.log(self.viewTemplateHtml)
-          for (var i = 0; i < process.inputProperty[0].entityschema.entity.length; i++) {
-            let element = process.inputProperty[0].entityschema.entity[i].name
-            console.log('-1->', element)
-            // element = element.toLowerCase()
-            // element = _.capitalize(element)
-            console.log('0000', document.getElementsByName(element.toLowerCase())[0])
-            console.log('1111', processLog.input[0][_.capitalize(element)])
-            document.getElementsByName(element.toLowerCase())[0].val = 'hello'
-            // console.log('2222', $('input[name="' + element.toLowerCase() + '"]').val())
-            $('input[name="' + element.toLowerCase() + '"]').each(function () {
-              console.log('-2->', this)
-              // this.val = processLog.input[0][element]
-            })
-          }
         })
       })
       .catch(function (error) {
         self.$Notice.error({title: 'Error..!', desc: 'Something went wrong please try after some time.'})
         console.log('Error : ', error)
       })
+    this.fillForm(processLog)
   }
 }
 </script>
