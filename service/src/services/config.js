608e563860fe3bd8d9865ec7bbd63a3c2bdd34c4
Krunal <kmahera@officebrain.com>
2017-12-07 11:22:43 +0530

    added certificate

--- service/src/services/config.js
+++ service/src/services/config.js
@@ -1,11 +1,16 @@
 const config = require('config');
-console.log('USER', config.get('rdb_user').trim());
+const fs = require('fs');
+// console.log('USER', config.get('rdb_user').trim());
+let ssl = process.env.cert ? { ca: fs.readFileSync(__dirname + process.env.cert) } : null
+let rauth = process.env.rauth ? process.env.rauth : null
 var rethinkdb = {
   rethinkdb: {
     db: 'FlowzEngine',
     servers: [{
       host: config.get('rdb_host').trim(),
-      port: config.get('rdb_port').trim()
+      port: config.get('rdb_port').trim(),
+      authKey: rauth,
+      ssl: ssl
     }]
   }
 };
