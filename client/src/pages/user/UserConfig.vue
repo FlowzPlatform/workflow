a0c016feb3688e9deccc149286f8615d88a44d89
Sanjaysinh Zala <sanjaysinhz@officebrain.com>
2017-12-08 18:10:47 +0530

    implementing user config

---
+++ client/src/pages/user/UserConfig.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <Row>
+        <Col span="12" style="padding-right:10px">
+            <Select v-model="selected" filterable>
+                <Option v-for="item in userList" :value="item.value" :key="item.value">{{ item.label }}</Option>
+            </Select>
+        </Col>
+      </Row>  
+    </div>
+</template>
+<script>
+import config from '@/config'
+export default {
+  data () {
+    return {
+      userList: [],
+      selected: ''
+    }
+  },
+  mounted () {
+    console.log(config.serverURI)
+  }
+}
+</script>
