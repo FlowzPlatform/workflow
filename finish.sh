d0132dcba32fff8493f6c92f0d7d77a5e4ed5e1f
Rohit Bajaniya <rbajaniya@officebrain.com>
2017-12-22 12:06:01 +0530

    Change devops files

--- finish.sh
+++ finish.sh
@@ -2,9 +2,9 @@ curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -X POST \
 'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s239?action=finishupgrade'
 
-curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
--X POST \
-'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s316?action=finishupgrade'
+# curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
+# -X POST \
+# 'http://rancher.flowz.com:8080/v2-beta/projects/1a29/services/1s316?action=finishupgrade'
 
 curl -u ""$RANCHER_USER":"$RANCHER_PASS"" \
 -X POST \
