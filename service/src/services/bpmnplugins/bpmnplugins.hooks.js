f7db4e48396e2acca81be1b227e4165a88c52fd9
nikitam <nikitam@officebrain.com>
2018-01-23 12:05:05 +0530

    plugins uploded through files

--- service/src/services/bpmnplugins/bpmnplugins.hooks.js
+++ service/src/services/bpmnplugins/bpmnplugins.hooks.js
@@ -32,24 +32,25 @@ module.exports = {
     all: [],
     find: [],
     get: [],
-    create: [
-      hook => sendError(hook)
-    ],
+    create: [],
     update: [],
     patch: [],
     remove: []
   }
 };
+
 var schemaequalChecker = function(item) {
   var allschema = await (getallSchema())
   var flag = false
   for (let [i, obj] of allschema.entries()) {
     var sid = obj.id
-    delete obj._id
+    // delete obj._id
     delete obj.id
     delete obj.createTemplate
     delete obj.emailTemplate
     delete obj.viewTemplate
+    delete obj.createdAt
+    delete obj.isdeleted
     var s = _.isEqual(obj, item)
     if (s) {
       return { status: s, id: sid }
@@ -66,10 +67,10 @@ var checkpulginexist = async(function(sdata) {
     }
   }
   if (!flag) {
-    // console.log('Not..Exist')
+    console.log('Plugin Not..Exist')
     return { status: false }
   } else {
-    // console.log('Exist')
+    console.log('Plugin Exist')
     return { status: true }
   }
 })
@@ -79,6 +80,7 @@ var sendError = function(hook) {
 var beforeCreate = async(function(hook) {
   // console.log('Hook Data.......................', hook.data)
   var n = await (checkpulginexist(hook.data))
+  // console.log('checkpulginexist::: ', n)
     // console.log('checkpulginexist...................', n)
   if (n.status) {
     // throw new Error('Message text can not be empty')
@@ -88,25 +90,28 @@ var beforeCreate = async(function(hook) {
       // hook.error = new errors.Conflict('This entry already exists');
       // return hook
   } else {
+    // console.log('Here.....................')
     for (let [inx, iObj] of hook.data.input.entries()) {
       // iObj['i_id'] = iObj.id
       var s = await (schemaequalChecker(iObj.entityschema[0]))
-        // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> input', s)
       if (s.status) {
+        console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> input isEqual :: ', s.id)
         iObj.entityschema = s.id
       } else {
         var res = await (checkSchemaObj(iObj.entityschema[0]))
+        // console.log('>>>>>>>>>>>>>>>> :: ', res, '  :: >>>>>>>>>>>>>')
         iObj.entityschema = res
       }
     }
     for (let [inx, oObj] of hook.data.output.entries()) {
       for (var [i, obj] of oObj.entityschema.entries()) {
         var s = await (schemaequalChecker(obj))
-          // console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> output', s)
         if (s.status) {
+          console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> output isEqual :: ', s.id)
           oObj.entityschema = s.id
         } else {
           var s = await (checkSchemaObj(obj))
+          // console.log('>>>>>>>>>>>>>>>> :: ', res, '  :: >>>>>>>>>>>>>')
           oObj.entityschema = s
         }
       }
@@ -114,6 +119,7 @@ var beforeCreate = async(function(hook) {
     hook.data['pluginType'] = hook.data.pluginType
     hook.data['image'] = hook.data.image
       // hook.data['imgurl'] = ""
+    // console.log('..................1111111111111111111111', hook.data)
     let workerobject = {}
     if (hook.data.worker == undefined) {
       workerobject.type = 'text/javascript'
@@ -123,36 +129,47 @@ var beforeCreate = async(function(hook) {
       let jsdata = await (axios.get(hook.data.worker.src))
       workerobject.src = jsdata.data
     }
+    // console.log('..................22222222222222222222222')
     hook.data['worker'] = workerobject
     let registerprocess = await (registerWorkerProcess(hook.data, workerobject.src))
+      // console.log('..................333333333333333333333')
     hook.data['createdOn'] = new Date()
     hook.data['isEnable'] = true
     hook.data['url'] = ""
     delete hook.data.type
     delete hook.data.imageStr
+    // console.log('hook.data.............. Final...', hook.data)
   }
 })
 var registerWorkerProcess = async(function(obj, src) {
+  // console.log('inside registerWorkerProcess', obj, '  ::src:: ' ,src)
   let workerparameter = {}
   workerparameter.jobtype = obj.pluginType + '_worker'
   workerparameter.jobprocess = src
   var form = new FormData();
   form.append("jobtype", obj.pluginType + '_worker');
   form.append("jobprocess", src);
-  const config = { headers: form.getHeaders() };
-  let registeredWorker = await (axios.post(config.registerWokerAPI, form, config))
+  const config1 = { headers: form.getHeaders() };
+  // console.log('inner 111111111111111111', config.registerWokerAPI)
+  let registeredWorker = await (axios.post(config.registerWokerAPI, form, config1))
+  // console.log('inner 222222222222222222')
+
   if (registeredWorker.err) {
     return undefined
   } else {
+    // console.log('inner 333333333333333')
     let callputmethod = await (axios.put(config.registerProcessAPI + obj.pluginType + '_worker'));
     return registeredWorker
   }
+  // console.log('inner 444444444444444')
   return registeredWorker
 })
 var checkSchemaObj = async(function(obj) {
   obj['createTemplate'] = []
   obj['emailTemplate'] = []
   obj['viewTemplate'] = []
+  obj['isdeleted'] = false
+  obj['createdAt'] = new Date()
   var flag = await (checkFlag(obj))
   if (!flag) {
     var res = await (saveSchemaObj(obj))
@@ -165,6 +182,7 @@ var checkSchemaObj = async(function(obj) {
     }
   }
   var res = await (saveSchemaObj(obj))
+  // console.log('.......res.......', res)
   return res
 })
 var checkFlag = function(obj) {
@@ -178,9 +196,12 @@ var checkFlag = function(obj) {
 }
 var saveSchemaObj = async(function(obj) {
   var res = await (axios.post(serverUrl + '/schema', obj))
-  return res.data
+  console.log('......................................................id:::: ', res.data.id)
+  return res.data.id
 })
 var getallSchema = async(function() {
-  var res = await (axios.get(serverUrl + '/schema'))
-  return res.data
+  var count = await(axios.get(serverUrl + '/schema?$limit=0'))
+  var limit = count.data.total
+  var res = await (axios.get(serverUrl + '/schema?limit='+ limit ))
+  return res.data.data
 })
