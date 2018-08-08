69d5c8073bf16ea51d59dfff1f1767dcd9a6dcfd
Faizan Saiyed <fsaiyed@officebrain.com>
2018-08-08 20:10:14 +0530

    Updated schema form

---
+++ client/src/components/mycustom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="mycustom">
+  	My ALSKAJS
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'mycustom'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
