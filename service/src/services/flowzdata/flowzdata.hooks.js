297cb58453bb5104eb5d07b74d71b1b85c52e24d
chirag <chirag@officebrain.com>
2018-08-22 17:08:47 +0530

    set modified at in finstance

--- service/src/services/flowzdata/flowzdata.hooks.js
+++ service/src/services/flowzdata/flowzdata.hooks.js
@@ -38,18 +38,18 @@ module.exports = {
 };
 
 function beforeCreate (hook) {
-  hook.data.createdAt = new Date().toISOString()
-  hook.params.isdone = true
+  hook.data.createdAt = new Date().toISOString();
+  hook.params.isdone = true;
   if (hook.data.hasOwnProperty('nextTarget')) {
-    hook.params.nextTarget = hook.data.nextTarget
-    delete hook.data.nextTarget
+    hook.params.nextTarget = hook.data.nextTarget;
+    delete hook.data.nextTarget;
   }
 }
 
 function afterCreate (hook) {
   // console.log('_________________________', hook.result.id)
   if (hook.params.hasOwnProperty('isdone') && hook.params.isdone) {
-    console.log('------', hook.result.id, '------')
+    console.log('------', hook.result.id, '------');
     hook.params.query = {};
     hook.params.query.$select = ['json'];
     const query = Object.assign({}, hook.params.query);
@@ -64,16 +64,17 @@ function afterCreate (hook) {
           userId: hook.params.userPackageDetails
         };
         if (hook.params.hasOwnProperty('nextTarget')) {
-          mdata.currentStatus = hook.params.nextTarget
+          mdata.currentStatus = hook.params.nextTarget;
         }
-        console.log('mdata', mdata)
+        console.log('mdata', mdata);
         mdata.stageReference.push({
           StageName: finstRes.currentStatus,
           stageRecordId: hook.result.id,
-          createdAt: new Date().toISOString()
-        })
+          createdAt: new Date().toISOString(),
+          userId: hook.params.userPackageDetails
+        });
         if (nextTargetObj.type === 'endevent') {
-          mdata.mainStatus = 'completed'
+          mdata.mainStatus = 'completed';
         }
         return hook.app.service('finstance').patch(hook.data.iid, mdata).then(pRes => {
           return hook;
@@ -81,28 +82,28 @@ function afterCreate (hook) {
           throw new errors.BadRequest('Error', {
             errors: { message: err.toString() }
           });
-        })
+        });
       }).catch(err => {
         throw new errors.BadRequest('Error', {
           errors: { message: err.toString() }
         });
-      })
+      });
       return hook;
     }).catch(err => {
       throw new errors.BadRequest('Error', {
         errors: { message: err.toString() }
       });
-    })
+    });
   }
 }
 
 function getNextTarget (processList, targetId) {
-  let targetObj = _.find(processList,{"id": targetId})
+  let targetObj = _.find(processList,{'id': targetId});
   if (targetObj.type === 'start' || targetObj.type === 'endevent' || targetObj.type === 'intermediatethrowevent') {
     return targetObj;
   }
   if(targetObj.inputProperty.length === 0) {
-    targetObj = getNextTarget(instanceId, targetObj.target[0].id)
+    targetObj = getNextTarget(instanceId, targetObj.target[0].id);
   }
   return targetObj;
-};
+}
