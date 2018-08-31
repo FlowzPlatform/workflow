46a40377481d8fb468508c31d6006901e3c81abc
harshp <harshp@officebrain.com>
2018-08-31 18:18:51 +0530

    Code Optimization

--- client/static/bpmn/bpmn-js-properties-panel/lib/provider/bpmn/parts/ExecutableProps.js
+++ client/static/bpmn/bpmn-js-properties-panel/lib/provider/bpmn/parts/ExecutableProps.js
@@ -17,24 +17,24 @@ module.exports = function(group, element, translate) {
 
   if (is(element, 'bpmn:Process') || (is(element, 'bpmn:Participant') && bo.get('processRef'))) {
 
-    var executableEntry = entryFactory.checkbox({
-      id: 'process-is-executable',
-      label: translate('Executable'),
-      modelProperty: 'isExecutable'
-    });
+  //   var executableEntry = entryFactory.checkbox({
+  //     id: 'process-is-executable',
+  //     label: translate('Executable'),
+  //     modelProperty: 'isExecutable'
+  //   });
 
-    // in participants we have to change the default behavior of set and get
-    if (is(element, 'bpmn:Participant')) {
-      executableEntry.get = function(element) {
-        return participantHelper.getProcessBusinessObject(element, 'isExecutable');
-      };
+  //   // in participants we have to change the default behavior of set and get
+  //   if (is(element, 'bpmn:Participant')) {
+  //     executableEntry.get = function(element) {
+  //       return participantHelper.getProcessBusinessObject(element, 'isExecutable');
+  //     };
 
-      executableEntry.set = function(element, values) {
-        return participantHelper.modifyProcessBusinessObject(element, 'isExecutable', values);
-      };
-    }
+  //     executableEntry.set = function(element, values) {
+  //       return participantHelper.modifyProcessBusinessObject(element, 'isExecutable', values);
+  //     };
+  //   }
 
-    group.entries.push(executableEntry);
+  //   group.entries.push(executableEntry);
   }
 
 };
