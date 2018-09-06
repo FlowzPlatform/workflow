33c9b1cf171fb239ca648eb5d6bcc1f40ea5cff9
nikitam <nikitam@officebrain.com>
2018-09-06 15:41:21 +0530

    issue-327

--- client/src/pages/flow/New.vue
+++ client/src/pages/flow/New.vue
@@ -17,7 +17,7 @@
             </SplitArea>
             <SplitArea :size="20" :minSize="200">
               <Tooltip content="Save" placement="left" class="upload-icon">
-                <a v-if="!btnLoading" @click="handleSave">
+                <a v-if="!btnLoading" @click="handleSubmit">
                   <i class="fa fa-floppy-o"></i>
                 </a>
                 <a v-if="btnLoading">
@@ -70,15 +70,188 @@
         btnLoading: false,
         processVar: null,
         bpmnModeler: null,
-        bpmnXML: '<?xml version="1.0" encoding="UTF-8"?><bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" id="Definitions_1" targetNamespace="http://bpmn.io/schema/bpmn"><bpmn:process id="Process_1" isExecutable="false"><bpmn:startEvent id="StartEvent_1" /></bpmn:process><bpmndi:BPMNDiagram id="BPMNDiagram_1"><bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1"><bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1"><dc:Bounds x="173" y="102" width="36" height="36" /></bpmndi:BPMNShape></bpmndi:BPMNPlane></bpmndi:BPMNDiagram></bpmn:definitions>'
+        bpmnXML: '<?xml version="1.0" encoding="UTF-8"?><bpmn:definitions xmlns:bpmn="http://www.omg.org/spec/BPMN/20100524/MODEL" xmlns:bpmndi="http://www.omg.org/spec/BPMN/20100524/DI" xmlns:di="http://www.omg.org/spec/DD/20100524/DI" xmlns:dc="http://www.omg.org/spec/DD/20100524/DC" id="Definitions_1" targetNamespace="http://bpmn.io/schema/bpmn"><bpmn:process id="Process_1" isExecutable="false"><bpmn:startEvent id="StartEvent_1" /></bpmn:process><bpmndi:BPMNDiagram id="BPMNDiagram_1"><bpmndi:BPMNPlane id="BPMNPlane_1" bpmnElement="Process_1"><bpmndi:BPMNShape id="_BPMNShape_StartEvent_2" bpmnElement="StartEvent_1"><dc:Bounds x="173" y="102" width="36" height="36" /></bpmndi:BPMNShape></bpmndi:BPMNPlane></bpmndi:BPMNDiagram></bpmn:definitions>',
+        flowObject: {
+          name: '',
+          schema: 0,
+          startId: [],
+          endId: [],
+          intermediateId: [],
+          processList: {},
+          xml: '',
+          svg: ''
+        }
       }
     },
     methods: {
+      async handleSubmit () {
+        this.btnLoading = true
+        this.bpmnModeler.saveSVG({ format: true },
+          (e, svg) => {
+            this.flowObject.svg = svg
+          })
+        this.bpmnModeler.saveXML({ format: true },
+          (e, xml) => {
+            this.flowObject.xml = xml
+          })
+        let x2js = new X2JS()
+        let data = x2js.xml2js(this.flowObject.xml)
+        // console.log('data', data)
+        // Set Roles
+        let userRolesArr = []
+        if (data.definitions.process !== undefined) {
+          if (data.definitions.process._name !== undefined) {
+            // Set Process Name
+            this.flowObject.name = data.definitions.process._name
+            // Set Schema
+            if (data.definitions.process['_camunda:schema'] !== undefined) {
+              this.flowObject.schema = data.definitions.process['_camunda:schema']
+              if (this.flowObject.schema !== 0 && this.flowObject.schema !== '') {
+                if (data.definitions.process['_camunda:addedRoles']) {
+                  userRolesArr = data.definitions.process['_camunda:addedRoles'].split(',')
+                }
+                let jsonXML = data.definitions.process
+                // Set StartIds
+                if (jsonXML.startEvent !== undefined) {
+                  if (_.isArray(jsonXML.startEvent)) {
+                    this.flowObject.startId = _.map(jsonXML.startEvent, (d) => { return d._id })
+                  } else {
+                    this.flowObject.startId = [jsonXML.startEvent._id]
+                  }
+                }
+                // Set endIds
+                if (jsonXML.endEvent !== undefined) {
+                  if (_.isArray(jsonXML.endEvent)) {
+                    this.flowObject.endId = _.map(jsonXML.endEvent, (d) => { return d._id })
+                  } else {
+                    this.flowObject.endId = [jsonXML.endEvent._id]
+                  }
+                }
+                // Set intermediateThrowEvent Ids
+                if (jsonXML.intermediateThrowEvent !== undefined) {
+                  if (_.isArray(jsonXML.intermediateThrowEvent)) {
+                    this.flowObject.intermediateId = _.map(jsonXML.intermediateThrowEvent, (d) => { return d._id })
+                  } else {
+                    this.flowObject.intermediateId = [jsonXML.intermediateThrowEvent._id]
+                  }
+                }
+                // Set Process
+                let allProcess = []
+                for (let type in jsonXML) {
+                  if (type !== 'sequenceFlow') {
+                    if (typeof jsonXML[type] === 'object') {
+                      if (!Array.isArray(jsonXML[type])) {
+                        jsonXML[type] = [jsonXML[type]]
+                      }
+                      for (let m of jsonXML[type]) {
+                        // let obj = m
+                        m.type = type.toLowerCase()
+                        allProcess.push(m)
+                      }
+                    }
+                  }
+                }
+                this.flowObject.processList = null
+                this.flowObject.processList = {}
+                if (allProcess.length > 0) {
+                  if (allProcess.length === 1) {
+                    for (let type in jsonXML) {
+                      if (type !== 'sequenceFlow') {
+                        if (typeof jsonXML[type] === 'object') {
+                          if (!Array.isArray(jsonXML[type])) {
+                            jsonXML[type] = [jsonXML[type]]
+                          }
+                          for (let [inx, m] of jsonXML[type].entries()) {
+                            this.flowObject.processList[m._id] = {
+                              id: m._id,
+                              name: m._name || '',
+                              type: type,
+                              order: inx,
+                              target: this.getTargetId(m, jsonXML)
+                            }
+                          }
+                        }
+                      }
+                    }
+                  } else {
+                    for (let [inx, item] of data.definitions.BPMNDiagram.BPMNPlane.BPMNShape.entries()) {
+                      let m = _.find(allProcess, {_id: item._bpmnElement})
+                      if (m !== undefined && m !== null) {
+                        this.flowObject.processList[m._id] = {
+                          id: m._id,
+                          name: m._name || '',
+                          type: m.type,
+                          order: inx,
+                          target: this.getTargetId(m, jsonXML)
+                        }
+                      }
+                    }
+                  }
+                  let actions = []
+                  let filteredProcesses = _.filter(allProcess, (o) => {
+                    if (o.type !== 'startevent' && o.type !== 'endevent' && o.type !== 'intermediatethrowevent') {
+                      return o
+                    }
+                  })
+
+                  for (var i = 0; i < filteredProcesses.length; i++) {
+                    actions.push(filteredProcesses[i]._id)
+                  }
+
+                  let actionsObj = {}
+                  for (let i = 0; i < actions.length; i++) {
+                    actionsObj[actions[i]] = this.permissions
+                  }
+                  subscriptionNew.moduleResource.moduleName = 'workflow_' + this.$route.params.id
+                  let registerAppModuleNew = actionsObj
+                  subscriptionNew.moduleResource.registerAppModule = registerAppModuleNew
+                  subscriptionNew.moduleResource.appRoles = userRolesArr
+                  subscriptionNew.registeredAppModulesRole().then(resp => {
+                    let result = null
+                    if (this.$route.params.id !== undefined) {
+                      result = flowz.put(this.$route.params.id, this.flowObject)
+                    } else {
+                      result = flowz.post(this.flowObject)
+                    }
+                    result.then(response => {
+                      this.$Notice.success({title: 'Success..!', desc: 'Flow Saved..'})
+                      this.$router.push({name: 'flow/list'})
+                      localStorage.removeItem('BPMNXml')
+                      this.btnLoading = false
+                    }).catch(error => {
+                      console.log(error)
+                      this.$Notice.error({title: 'Error..!', desc: 'Flow Not Saved...'})
+                      this.btnLoading = false
+                    })
+                  }).catch(err => {
+                    this.$Notice.error({title: 'Error..!', desc: 'Flow Not Saved. Try again.'})
+                    console.log('Error: ', err)
+                    this.btnLoading = false
+                  })
+                } else {
+                  this.$Message.error('Nothing to Save !')
+                  this.btnLoading = false
+                }
+              } else {
+                this.$Message.error('Please Add Schema for Flow !')
+                this.btnLoading = false
+              }
+            } else {
+              this.$Message.error('Please Add Schema for Flow !')
+              this.btnLoading = false
+            }
+          } else {
+            this.$Message.error('Please Add Process name !')
+            this.btnLoading = false
+          }
+        } else {
+          this.btnLoading = false
+        }
+      },
       async handleSave () {
         this.btnLoading = true
         let xmlData
         let svgData = ''
-        // console.log('this.bpmnModeler', this.bpmnModeler)
         this.bpmnModeler.saveSVG({ format: true },
           function (e, svg) {
             svgData = svg
@@ -90,7 +263,6 @@
         let x2js = new X2JS()
         let data = x2js.xml2js(xmlData)
 
-        // console.log('Final Data: Final Data:Final Data:Final Data: ', data.definitions.process['_camunda:addedRoles'])
         let userRoles = null
         if (data.definitions.process['_camunda:addedRoles']) {
           userRoles = data.definitions.process['_camunda:addedRoles']
@@ -108,9 +280,10 @@
         if (data.definitions.process._name !== undefined) {
           if (schema !== undefined && schema !== null && schema !== '' && schema !== '0') {
             let flowObject = {}
-            flowObject.ProcessName = data.definitions.process._name
+            // flowObject.ProcessName = data.definitions.process._name
+            flowObject.name = data.definitions.process._name
             flowObject.xml = xmlData
-            flowObject.roles = userRoles
+            // flowObject.roles = userRoles
             flowObject.schema = schema
             flowObject.json = await this.generateJson(xmlData)
 
@@ -147,6 +320,7 @@
               //   return m.value.split(',')
               // }).value()))
               let result = null
+              console.log('flowObject', flowObject)
               if (this.$route.params.id !== undefined) {
                 result = flowz.put(this.$route.params.id, flowObject)
               } else {
@@ -238,10 +412,11 @@
         let self = this
         let x2js = new X2JS()
         let jsonXML = x2js.xml2js(xml)
+        console.log('jsonXML', jsonXML)
         jsonXML = jsonXML.definitions.process
         // console.log('jsonXML', jsonXML)
         let instanceObject = {}
-        instanceObject.name = jsonXML._name
+        // instanceObject.name = jsonXML._name
         instanceObject.start_delay = 3000
         // Add Start Events
         if (_.isArray(jsonXML.startEvent)) {
@@ -355,7 +530,7 @@
             capacity: (m._isFormInput) ? m._capacity : false,
             isProcessTask: m._isProcessTask !== undefined ? (m._isProcessTask === 'true') : (m['_camunda:isProcessTask'] === 'true'),
             executeAny: m['_camunda:executeIfAny'] !== undefined ? ((m['_camunda:executeIfAny']) ? m['_camunda:countany'] : false) : false,
-            name: m._name,
+            name: m._name || '',
             type: 'start',
             target: self.getTargetId(m, process)
             // mapping: [],
@@ -366,29 +541,33 @@
         }).value())
       },
       getTargetId (event, process) {
-        if (!_.isArray(event.outgoing)) {
-          event.outgoing = [event.outgoing]
+        if (event.hasOwnProperty('outgoing')) {
+          if (!_.isArray(event.outgoing)) {
+            event.outgoing = [event.outgoing]
+          }
+          return _.map(event.outgoing, (targetMap) => {
+            return _.chain(process.sequenceFlow).filter((ftr) => {
+              // console.log('ftr._id', ftr._id, targetMap)
+              return ftr._id === targetMap.__text
+            }).map((m) => {
+              if (m._name !== undefined && m._name !== '') {
+                return {
+                  label: m._name,
+                  id: m._targetRef
+                  // outputid: m.extensionElements !== undefined ? m.extensionElements.myIOMapping.mapping._producer : ''
+                }
+              } else {
+                return {
+                  id: m._targetRef
+                  // outputid: m.extensionElements !== undefined ? m.extensionElements.myIOMapping.mapping._producer : ''
+                }
+              }
+            }).value()[0]
+            // return { id: targetMap.__text }
+          })
+        } else {
+          return []
         }
-        return _.map(event.outgoing, (targetMap) => {
-          return _.chain(process.sequenceFlow).filter((ftr) => {
-            // console.log('ftr._id', ftr._id)
-            return ftr._id === targetMap.__text
-          }).map((m) => {
-            if (m._name !== undefined && m._name !== '') {
-              return {
-                label: m._name,
-                id: m._targetRef,
-                outputid: m.extensionElements !== undefined ? m.extensionElements.myIOMapping.mapping._producer : ''
-              }
-            } else {
-              return {
-                id: m._targetRef,
-                outputid: m.extensionElements !== undefined ? m.extensionElements.myIOMapping.mapping._producer : ''
-              }
-            }
-          }).value()[0]
-          // return { id: targetMap.__text }
-        })
       },
       getConfigurationsProperties (proccess) {
         // console.log('process..config', proccess.extensionElements.myConfigurations)
