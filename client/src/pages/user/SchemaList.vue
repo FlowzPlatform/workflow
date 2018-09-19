086c6af69e85514dfb23fad2bfa9e134d6e8f93c
harshp <harshp@officebrain.com>
2018-09-19 10:37:47 +0530

    check if permission.otherwise display all data

--- client/src/pages/user/SchemaList.vue
+++ client/src/pages/user/SchemaList.vue
@@ -219,19 +219,36 @@
         this.dataConfig = []
         if (this.schema.hasOwnProperty('entity')) {
           for (let item of this.schema.entity) {
-            if (this.schema.permission[item.name].read === true) {
-              let isshow = true
-              if (item.customtype) {
-                isshow = false
+            if (this.schema.hasOwnProperty('permission')) {
+              if (this.schema.permission !== undefined) {
+                if (this.schema.permission[item.name].read === true) {
+                  let isshow = true
+                  if (item.customtype) {
+                    isshow = false
+                  }
+                  this.dataConfig.push({
+                    title: item.name,
+                    key: item.name,
+                    show: isshow,
+                    sortable: false,
+                    type: item.type,
+                    width: 150
+                  })
+                }
+              } else {
+                let isshow = true
+                if (item.customtype) {
+                  isshow = false
+                }
+                this.dataConfig.push({
+                  title: item.name,
+                  key: item.name,
+                  show: isshow,
+                  sortable: false,
+                  type: item.type,
+                  width: 150
+                })
               }
-              this.dataConfig.push({
-                title: item.name,
-                key: item.name,
-                show: isshow,
-                sortable: false,
-                type: item.type,
-                width: 150
-              })
             }
           }
         }
