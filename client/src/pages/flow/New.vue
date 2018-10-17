c2c60aecdfd3e22225b8d2336689e5eea696d7c9
harshp <harshp@officebrain.com>
2018-10-17 14:40:20 +0530

    subscription id added

--- client/src/pages/flow/New.vue
+++ client/src/pages/flow/New.vue
@@ -394,7 +394,8 @@
       initFlow () {
         let tempVar = []
         saveemailTemplate.get(null, {
-          userId: this.$store.state.user._id
+          userId: this.$store.state.user._id,
+          subscriptionId: this.$store.state.subscription
         })
         .then((res) => {
           // tempVar = res.data.data
@@ -411,7 +412,8 @@
           new Promise((resolve, reject) => {
             schemaModel.get(null, {
               $paginate: false,
-              isdeleted: false
+              isdeleted: false,
+              subscriptionId: this.$store.state.subscription
             }).then((response) => {
               response.data.splice(0, 0, { title: '---select---', id: 0 })
               resolve(response.data)
